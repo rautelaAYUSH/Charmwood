ab22e9462b3941dc7f5790b42988dd633a14eaa7
maletha <anurag.maletha007@gmail.com>
2024-08-13 22:29:16 +0530

    fix page shrink issue when page width is below 320

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,10 +4,25 @@ import PhoneIcon from "@mui/icons-material/Phone";
 import EmailIcon from "@mui/icons-material/Email";
 import CommonTextfield from "./reusableComponents/commonTextfield";
 import { useTheme } from "@emotion/react";
+import { useEffect, useState } from "react";
 
 const Footer = () => {
   const theme = useTheme();
   const isLargeScreen = useMediaQuery(theme.breakpoints.up("lg"));
+  const [isXs, setIsXs] = useState(window.innerWidth <= 400);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsXs(window.innerWidth <= 400);
+    };
+
+    window.addEventListener("resize", handleResize);
+    
+    // Cleanup listener on unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <Box
@@ -20,24 +35,39 @@ const Footer = () => {
     >
       <Grid container spacing={4}>
         <Grid item xs={12} md={6}>
-          <Typography variant="h4" gutterBottom sx={{fontSize:'2rem', fontFamily: 'Playfair Display, serif'}}>
+          <Typography
+            variant="h4"
+            gutterBottom
+            sx={{ fontSize: "2rem", fontFamily: "Playfair Display, serif" }}
+          >
             Contact Us
           </Typography>
-          <Typography variant="body1" sx={{ marginBottom: "1rem", fontSize:'1.1rem', fontFamily:'Inter, sans-serif'}}>
+          <Typography
+            variant="body1"
+            sx={{
+              marginBottom : "1rem",
+              fontSize     : "1.1rem",
+              fontFamily   : "Inter, sans-serif",
+            }}
+          >
             We are here to address your inquiries and provide the help you need.
           </Typography>
-          <Typography variant="body2" sx={{fontSize:'1rem', fontFamily:'Inter, sans-serif'}}>
+          <Typography
+            variant="body2"
+            sx={{ fontSize: "1rem", fontFamily: "Inter, sans-serif" }}
+          >
             <Box
               component="span"
               sx={{
                 display      : "flex",
                 alignItems   : "center",
                 marginBottom : "1rem",
-                fontSize: "1rem",
+                fontSize     : "1rem",
               }}
             >
               <LocationOnIcon sx={{ marginRight: "0.5rem" }} />
-              Behind Indian oil petrol pu.p, Kidduwala, Dehradun, Uttarakhand 248008
+              Behind Indian oil petrol pump, Kidduwala, Dehradun, Uttarakhand
+              248008
             </Box>
             <Box
               component="span"
@@ -45,7 +75,7 @@ const Footer = () => {
                 display      : "flex",
                 alignItems   : "center",
                 marginBottom : "1rem",
-                fontSize: "1rem",
+                fontSize     : "1rem",
               }}
             >
               <PhoneIcon sx={{ marginRight: "0.5rem" }} />
@@ -57,12 +87,25 @@ const Footer = () => {
                 display      : "flex",
                 alignItems   : "center",
                 marginBottom : "1rem",
-                fontSize: "1rem",
-                fontFamily:'Inter, sans-serif'
+                fontSize     : "1rem",
+                fontFamily   : "Inter, sans-serif",
               }}
             >
               <EmailIcon sx={{ marginRight: "0.5rem" }} />
-              charmwoodhorseriding@gmail.com
+              {isXs ? (
+                <a
+                  href="https://mail.google.com/mail/?view=cm&fs=1&to=charmwoodhorseriding@gmail.com"
+                  target="_blank"
+                  style={{
+                    textDecoration : "none",
+                    color          : "inherit",
+                  }}
+                >
+                  click to mail
+                </a>
+              ) : (
+                "charmwoodhorseriding@gmail.com"
+              )}
             </Box>
           </Typography>
         </Grid>
@@ -74,7 +117,7 @@ const Footer = () => {
               padding         : "1rem",
               borderRadius    : "8px",
               color           : "black",
-              fontFamily:'Inter, sans-serif',
+              fontFamily      : "Inter, sans-serif",
             }}
           >
             <CommonTextfield label={"Full Name"} />
@@ -92,7 +135,7 @@ const Footer = () => {
               sx={{
                 backgroundColor : "#000",
                 color           : "#fff",
-                fontFamily:'Inter, sans-serif',
+                fontFamily      : "Inter, sans-serif",
                 marginTop       : "1rem",
                 "&:hover"       : {
                   backgroundColor: "#333",
